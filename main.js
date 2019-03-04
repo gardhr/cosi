0895be783a8ca3160b681d62f9a72ef1bee5621e
gardhr <24468810+gardhr@user.noreply.github.com>
2019-03-04 08:17:26 -0400

    update examples

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ function print_mechanical(pace, text)
 var text = file_to_text(script_path())
 var args = argv_to_text_array()
 if(!args.length)
- args = [2/10]
+ args = [1/5]
 loop(args, function(idx)
 {
  var pace = Number(args[idx])
