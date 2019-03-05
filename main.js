3a1dc7f6217b97e0aba2e9058d810d6e2532c0b2
gardhr <24468810+gardhr@user.noreply.github.com>
2019-03-04 20:38:40 -0400

    update examples

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 print("Sample Cosi Application")
 print("(NOTE: default filename is 'main.js')")
 
@@ -6,10 +7,10 @@ function tick()
  return clock() / CLOCKS_PER_SEC
 }
 
-function wait(pause)
+function wait(delay)
 {
- var tock = tick() + pause
- while(tick() < tock)
+ var rest = tick() + delay
+ while(tick() < rest)
   continue
 }
 
@@ -18,32 +19,34 @@ function print_mechanical(pace, text)
  srand(time(NULL))
  if(pace > 1)
   pace = 1 / pace
+
  function put_mechanical(byte)
  {
   var modulated = pace * (rand() / RAND_MAX)
   putchar(byte)
   fflush(stdout)
   wait(modulated) 
- } 
- var all = "";
+ }
+ 
+ var combined = "";
  for(var idx = 1; idx < arguments.length; ++idx)
-  all += arguments[idx]
- var bytes = text_to_bytes(all) 
- loop(all, function(jdx)
+  combined += arguments[idx]
+ var bytes = text_to_bytes(combined) 
+ loop(combined, function(idx)
  {
-  put_mechanical(get_byte(bytes, jdx))
+  put_mechanical(get_byte(bytes, idx))
  })
  put_mechanical(0xa)
  free(bytes)
 }
 
-var text = file_to_text(script_path())
 var args = argv_to_text_array()
 if(!args.length)
- args = [1/5]
+ args = [1/4]
+var text = file_to_text(script_path())
 loop(args, function(idx)
 {
  var pace = Number(args[idx])
- print("*** Script ***")
+ print("*** Script (printed with", pace, "second delays) ***")
  print_mechanical(pace, text) 
 })
