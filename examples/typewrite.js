0c4b5765fddbba53f2750297f0c89ea4366a0f37
gardhr <24468810+gardhr@users.noreply.github.com>
2020-01-04 17:19:58 -0400

    update example

--- examples/typewrite.js
+++ examples/typewrite.js
@@ -1,20 +1,17 @@
 srand(time(NULL))
 
 function typewrite(delay, text)
-{
- 
+{ 
  function tick()
  {
   return clock() / CLOCKS_PER_SEC
  }
-
  function wait(delay)
  { 
   var later = tick() + delay
   while(tick() < later)
    continue
  }
-
  function typebyte(byte)
  {
   var modulated = delay * (rand() / RAND_MAX)
@@ -22,7 +19,6 @@ function typewrite(delay, text)
   fflush(stdout)
   wait(modulated) 
  }
- 
  if(delay > 1)
   delay = 1 / delay
  var combined = "";
@@ -41,16 +37,18 @@ function typewrite(delay, text)
   Typewrite!  
 */
 
-var args = script_arguments()
-if(!args.length)
+var args = script_arguments(),
+ length = args.length
+if(length == 0)
  args = [script_path()]
-loop(args, function(idx)
-{
- var arg = args[idx]
- var delay = rand() % 48, 
-  text = file_to_text(arg) || file_to_text(arg + ".js")
- if(text)
-  typewrite(delay, text)
- else
-  typewrite(delay, "Error: cannot load file '", arg, "'") 
-})
+if(length < 2)
+ args.push(3) 
+var file = args[0],
+ pause = args[1] | 0, 
+ text = file_to_text(file) || 
+  file_to_text(file + ".js")
+if(text)
+ typewrite(pause, text)
+else
+ typewrite(pause, 
+  "Error: cannot load file '", file, "'")
